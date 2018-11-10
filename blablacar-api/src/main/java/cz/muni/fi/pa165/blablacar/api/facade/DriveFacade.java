54e9f9d5fd691dcc54a3eb93129975022f120fa0
samuelmacko <samuel.macko.sm@gmail.com>
2018-11-10 16:01:34 +0100

    Added Drive facade and some empty DTOs

--- blablacar-api/src/main/java/cz/muni/fi/pa165/blablacar/api/facade/DriveFacade.java
+++ blablacar-api/src/main/java/cz/muni/fi/pa165/blablacar/api/facade/DriveFacade.java
@@ -1,4 +1,83 @@
 package cz.muni.fi.pa165.blablacar.api.facade;
 
-public class DriveFacade {
+import cz.muni.fi.pa165.blablacar.api.dto.*;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Facade interface for Drive
+ *
+ * @author Samuel Macko
+ */
+public interface DriveFacade {
+
+    /**
+     * Create a drive
+     *
+     * @param driveCreateDTO parameters of a Drive
+     * @return id of a drive
+     */
+    Long createDrive(DriveCreateDTO driveCreateDTO);
+
+    /**
+     * Delete a drive
+     *
+     * @param id id of a drive
+     */
+    void deleteDrive(Long id);
+
+    /**
+     * Add a customer to a drive
+     *
+     * @param addCustomerDTO parameters of a new customer
+     */
+    void addCustomer(AddCustomerDTO addCustomerDTO);
+
+    /**
+     * Remove a customer
+     *
+     * @param removeCustomerDTO parameters of a customer
+     */
+    void removeCustomer(RemoveCustomerDTO removeCustomerDTO);
+
+    /**
+     * Change a capacity
+     *
+     * @param id id of a drive
+     * @param newCapacity capacity to be assigned
+     */
+    void changeCapacity(Long id, int newCapacity);
+
+    /**
+     * Change a driver
+     *
+     * @param id id of a drive
+     * @param changeDriverDTO parameters of a driver
+     */
+    void changeDriver(Long id, ChangeDriverDTO changeDriverDTO);
+
+    /**
+     * Change a date
+     *
+     * @param id id of a drive
+     * @param newDate date to be assigned
+     */
+    void changeDate(Long id, Date newDate);
+
+    /**
+     * Find a drive by a drive id
+     *
+     * @param id id of a drive
+     * @return drive with a specified id or null if not found
+     */
+    DriveDTO findDriveById(Long id);
+
+    /**
+     * Find add drives
+     *
+     * @return list of all drives in database
+     */
+    List<DriveDTO> findAllDrives();
+
 }
