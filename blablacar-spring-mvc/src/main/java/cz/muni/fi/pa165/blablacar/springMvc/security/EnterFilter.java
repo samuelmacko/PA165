8481c3f835d166a4bcbaa0c3e52f075086719eb5
martin.geletka <m.geletka@gmail.com>
2018-12-16 19:30:09 +0100

    added filter and userSession

---
+++ blablacar-spring-mvc/src/main/java/cz/muni/fi/pa165/blablacar/springMvc/security/EnterFilter.java
@@ -0,0 +1,46 @@
+package cz.muni.fi.pa165.blablacar.springMvc.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/user/*", "/ride/*", "/comment/*"})
+public class EnterFilter implements Filter {
+
+    @Autowired
+    private UserSession userSession;
+
+    private WebApplicationContext appContext;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        appContext = WebApplicationContextUtils.
+                getRequiredWebApplicationContext(filterConfig.getServletContext());
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        userSession = appContext.getBean(UserSession.class);
+
+        if (userSession.getUserId() == null) {
+            response401((HttpServletResponse) servletResponse);
+            return;
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+    private void response401(HttpServletResponse response) throws IOException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().println("<html><body><h1>401 Unauthorized</h1>You do not have permissions to access...</body></html>");
+    }
+}
